6e6ac10aa6e9f99db17aa4c1d4dfcca369d0ab69
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-01-19 13:38:47 -0600

    Adds starter project files.

---
+++ plantopedia/src/test/java/org/launchcode/plantopedia/PlantopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.plantopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
