44028e292dd21330419506fb58aa2c4c38bdf536
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-01-20 14:55:29 -0600

    New copy of project

---
+++ plantopedia/src/main/java/org/launchcode/plantopedia/PlantopediaApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.plantopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantopediaApplication.class, args);
+	}
+
+}
