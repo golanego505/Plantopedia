1c3458c8451ebd3947f828fa154bcd0e7d875722
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-01-26 11:58:47 -0600

    Refactors data type of returned array elements (from Plant and Species to PlantSpecies)

--- src/main/java/org/launchcode/plantopedia/models/responses/lists/PlantListResponse.java
+++ src/main/java/org/launchcode/plantopedia/models/responses/lists/PlantListResponse.java
@@ -1,17 +1,17 @@
 package org.launchcode.plantopedia.models.responses.lists;
 
-import org.launchcode.plantopedia.models.taxa.Plant;
+import org.launchcode.plantopedia.models.taxa.PlantSpecies;
 
 import java.util.List;
 
 public class PlantListResponse extends ListResponse {
-    private List<Plant> data;
+    private List<PlantSpecies> data;
 
-    public List<Plant> getData() {
+    public List<PlantSpecies> getData() {
         return data;
     }
 
-    public void setData(List<Plant> data) {
+    public void setData(List<PlantSpecies> data) {
         this.data = data;
     }
 }
--- src/main/java/org/launchcode/plantopedia/models/responses/lists/SpeciesListResponse.java
+++ src/main/java/org/launchcode/plantopedia/models/responses/lists/SpeciesListResponse.java
@@ -1,17 +1,17 @@
 package org.launchcode.plantopedia.models.responses.lists;
 
-import org.launchcode.plantopedia.models.taxa.Species;
+import org.launchcode.plantopedia.models.taxa.PlantSpecies;
 
 import java.util.List;
 
 public class SpeciesListResponse extends ListResponse {
-    private List<Species> data;
+    private List<PlantSpecies> data;
 
-    public List<Species> getData() {
+    public List<PlantSpecies> getData() {
         return data;
     }
 
-    public void setData(List<Species> data) {
+    public void setData(List<PlantSpecies> data) {
         this.data = data;
     }
 }
