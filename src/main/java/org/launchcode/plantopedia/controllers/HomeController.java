43fb749fe74a9c46f3dffdb7a38739e574c88556
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-01-27 16:42:26 -0600

    Links homepage cards to Trefle with client token.

--- src/main/java/org/launchcode/plantopedia/controllers/HomeController.java
+++ src/main/java/org/launchcode/plantopedia/controllers/HomeController.java
@@ -1,11 +1,16 @@
 package org.launchcode.plantopedia.controllers;
 
+import org.launchcode.plantopedia.models.responses.ClientTokenResponse;
 import org.launchcode.plantopedia.models.responses.lists.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Controller
 public class HomeController {
@@ -14,64 +19,15 @@ public class HomeController {
     @Value("${TREFLE_API_TOKEN}")
     private String apiKey;
     public static final String API_PATH ="https://trefle.io/api/v1/";
+    public static final String CLIENT_TOKEN_REQUEST_PATH = "https://trefle.io/api/auth/claim";
 
     @RequestMapping("/")
     public String index(Model model) {
+
         model.addAttribute("TREFLE_API_TOKEN", apiKey);
 
-        String listKingdomsUri = API_PATH + "kingdoms?token=" + apiKey;
-        String listSubkingdomsUri = API_PATH + "subkingdoms?token=" + apiKey;
-        String listDivisionsUri = API_PATH + "divisions?token=" + apiKey;
-        String listDivisionClassesUri = API_PATH + "division_classes?token=" + apiKey;
-        String listDivisionOrdersUri = API_PATH + "division_orders?token=" + apiKey;
-        String listFamiliesUri = API_PATH + "families?token=" + apiKey;
-        String listGeneraUri = API_PATH + "genus?token=" + apiKey;
         String listPlantsUri = API_PATH + "plants?token=" + apiKey;
-        String listSpeciesUri = API_PATH + "species?token=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
-
-        KingdomListResponse kingdomListResponse = restTemplate.getForObject(
-                listKingdomsUri, KingdomListResponse.class);
-        if (kingdomListResponse != null) {
-            model.addAttribute("kingdoms", kingdomListResponse.getData());
-        }
-
-        SubkingdomListResponse subkingdomListResponse = restTemplate.getForObject(
-                listSubkingdomsUri, SubkingdomListResponse.class);
-        if (subkingdomListResponse != null) {
-            model.addAttribute("subkingdoms", subkingdomListResponse.getData());
-        }
-
-        DivisionListResponse divisionListResponse = restTemplate.getForObject(
-                listDivisionsUri, DivisionListResponse.class);
-        if (divisionListResponse != null) {
-            model.addAttribute("divisions", divisionListResponse.getData());
-        }
-
-        DivisionClassListResponse divisionClassListResponse = restTemplate.getForObject(
-                listDivisionClassesUri, DivisionClassListResponse.class);
-        if (divisionClassListResponse != null) {
-            model.addAttribute("divisionClasses", divisionClassListResponse.getData());
-        }
-
-        DivisionOrderListResponse divisionOrderListResponse = restTemplate.getForObject(
-                listDivisionOrdersUri, DivisionOrderListResponse.class);
-        if (divisionOrderListResponse != null) {
-            model.addAttribute("divisionOrders", divisionOrderListResponse.getData());
-            //model.addAttribute("divisionOrderPaginationLinks", divisionOrderListResponse.getLinks());
-        }
-
-        FamilyListResponse familyListResponse = restTemplate.getForObject(
-                listFamiliesUri, FamilyListResponse.class);
-        if (familyListResponse != null) {
-            model.addAttribute("families", familyListResponse.getData());
-        }
-
-        GenusListResponse genusListResponse = restTemplate.getForObject(
-                listGeneraUri, GenusListResponse.class);
-        if (genusListResponse != null) {
-            model.addAttribute("genera", genusListResponse.getData());
-        }
 
         PlantListResponse plantListResponse = restTemplate.getForObject(
                 listPlantsUri, PlantListResponse.class);
@@ -79,12 +35,51 @@ public class HomeController {
             model.addAttribute("plants", plantListResponse.getData());
         }
 
-        SpeciesListResponse speciesListResponse = restTemplate.getForObject(
-                listSpeciesUri, SpeciesListResponse.class);
-        if (speciesListResponse != null) {
-            model.addAttribute("species", speciesListResponse.getData());
+        return "index";
+    }
+
+    @RequestMapping("get-token/{plantId}")
+    public String useClientToken(@PathVariable int plantId) {
+        String token = getClientToken().getToken();
+        return "redirect:https://trefle.io/api/v1/plants/" + plantId + "?token=" + token;
+    }
+
+    public ClientTokenResponse getClientToken() {
+        ClientTokenRequest request = new ClientTokenRequest("localhost:8080");
+        HttpEntity<ClientTokenRequest> entity = new HttpEntity<>(request);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject(URI.create(CLIENT_TOKEN_REQUEST_PATH + "?token=" + apiKey),
+                entity, ClientTokenResponse.class);
+    }
+
+    public static class ClientTokenRequest {
+        private String origin;
+        private String ip;
+
+        public ClientTokenRequest(String origin) {
+            this.origin = origin;
+            this.ip = null;
         }
 
-        return "index";
+        public ClientTokenRequest(String origin, String ip) {
+            this.origin = origin;
+            this.ip = ip;
+        }
+
+        public String getOrigin() {
+            return origin;
+        }
+
+        public void setOrigin(String origin) {
+            this.origin = origin;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
     }
 }
