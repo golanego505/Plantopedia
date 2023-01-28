6f1efdd2797b20a7d257415bf495daf316b29d43
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-01-27 23:20:32 -0600

    Implements list view of all plants with pagination

--- src/main/java/org/launchcode/plantopedia/controllers/HomeController.java
+++ src/main/java/org/launchcode/plantopedia/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.plantopedia.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.launchcode.plantopedia.models.responses.ClientTokenResponse;
 import org.launchcode.plantopedia.models.responses.lists.*;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class HomeController {
@@ -21,7 +26,7 @@ public class HomeController {
     public static final String API_PATH ="https://trefle.io/api/v1/";
     public static final String CLIENT_TOKEN_REQUEST_PATH = "https://trefle.io/api/auth/claim";
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
 
         String listPlantsUri = API_PATH + "plants?token=" + apiKey;
@@ -31,23 +36,64 @@ public class HomeController {
                 listPlantsUri, PlantListResponse.class);
         if (plantListResponse != null) {
             model.addAttribute("plants", plantListResponse.getData());
+            model.addAttribute("links", plantListResponse.getLinks());
+            model.addAttribute("meta", plantListResponse.getMeta());
         }
 
         return "index";
     }
 
-    @RequestMapping("plants/{plantId}")
-    public String plantDetails(@PathVariable int plantId) {
-        String token = getClientToken().getToken();
+    @RequestMapping(value = "/plants", method = RequestMethod.GET)
+    public String plantListResult(Model model, @RequestParam(value = "page", defaultValue = "0") String page) {
+
+        if (page.equals("0")) {
+            return "redirect:/";
+        }
+
+        String listPlantsUri = API_PATH + "plants?token=" + apiKey + "&page=" + page;
+        RestTemplate restTemplate = new RestTemplate();
+
+        PlantListResponse plantListResponse = restTemplate.getForObject(
+                listPlantsUri, PlantListResponse.class);
+        if (plantListResponse != null) {
+            model.addAttribute("plants", plantListResponse.getData());
+            model.addAttribute("links", plantListResponse.getLinks());
+            model.addAttribute("meta", plantListResponse.getMeta());
+        }
+
+        return "index";
+    }
+
+    @RequestMapping(value = "plants/{plantId}", method = RequestMethod.GET)
+    public String plantDetails(@PathVariable int plantId, HttpServletRequest request) {
+        String token = getClientToken(request.getRequestURI()).getToken();
         return "redirect:https://trefle.io/api/v1/plants/" + plantId + "?token=" + token;
     }
 
-    public ClientTokenResponse getClientToken() {
-        ClientTokenRequest request = new ClientTokenRequest("localhost:8080");
+    @RequestMapping(value = "parse-pagination-link", method = RequestMethod.GET)
+    public String parsePaginationLink(@RequestParam String link) {
+        String page = getPage(link);
+        return "redirect:/plants?page=" + page;
+    }
+
+    public ClientTokenResponse getClientToken(String origin) {
+        ClientTokenRequest request = new ClientTokenRequest(origin);
         HttpEntity<ClientTokenRequest> entity = new HttpEntity<>(request);
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject(URI.create(CLIENT_TOKEN_REQUEST_PATH + "?token=" + apiKey),
                 entity, ClientTokenResponse.class);
+    }
+
+    public String getPage(String uri) {
+        Pattern p = Pattern.compile("page=\\d+");
+        Matcher m = p.matcher(uri);
+        boolean b = m.find();
+        if (!b) {
+            return "0";
+        }
+        else {
+            return m.group().replace("page=", "");
+        }
     }
 
     public static class ClientTokenRequest {
