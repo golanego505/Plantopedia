5aae49c40c97d4b99219e285358a1912e4724dbb
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-02-01 06:59:43 -0600

    Implements search functionality from list view

--- src/main/java/org/launchcode/plantopedia/controllers/ToyController.java
+++ src/main/java/org/launchcode/plantopedia/controllers/ToyController.java
@@ -7,7 +7,6 @@ import org.launchcode.plantopedia.models.taxa.Genus;
 import org.launchcode.plantopedia.responses.lists.KingdomListResponse;
 import org.launchcode.plantopedia.responses.retrievals.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +21,7 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class ToyController {
     public static final String API_PATH ="https://trefle.io/api/v1/";
-
-    @SuppressWarnings("SpellCheckingInspection")
-    @Value("${TREFLE_API_TOKEN}")
-    private String apiKey;
+    private String apiKey = "MY_FAKE_KEY";
 
     @Autowired
     private FamilyRepository familyRepository;
