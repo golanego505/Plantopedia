41b020328f5ab10bdf121a8486704976d597bcfe
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2023-02-08 23:20:28 -0600

    Allows for ordering results by common name or scientific name.

--- src/main/java/org/launchcode/plantopedia/controllers/PlantListController.java
+++ src/main/java/org/launchcode/plantopedia/controllers/PlantListController.java
@@ -1,6 +1,7 @@
 package org.launchcode.plantopedia.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.launchcode.plantopedia.models.taxa.Plant;
 import org.launchcode.plantopedia.responses.ClientTokenResponse;
 import org.launchcode.plantopedia.responses.links.ListLinks;
 import org.launchcode.plantopedia.responses.lists.PlantListResponse;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,17 +36,31 @@ public class PlantListController {
         return "redirect:/plants";
     }
 
+    public String displayGivenListOfPlants(Model model, List<Plant> plants) {
+        model.addAttribute("plants", plants);
+        return "listPlants";
+    }
+
     @RequestMapping(value = "/plants", method = RequestMethod.GET)
     public String plantListByPage(Model model, @RequestParam(value = "page", defaultValue = "1") String page,
-                                  @RequestParam(defaultValue = "1") Boolean showImages,
+                                  @RequestParam(name = "showImages", defaultValue = "1") String showImages,
+                                  @RequestParam(name = "orderFieldOne", defaultValue = "") String orderFieldOne,
                                   @Value("${TREFLE_API_TOKEN}") String apiKey) {
 
         model.addAttribute("page", page);
+        model.addAttribute("showImages", !(showImages.equals("0") || showImages.equals("false")));
+        model.addAttribute("orderFieldOne", orderFieldOne);
         RestTemplate restTemplate = new RestTemplate();
-        URI listPlants = URI.create(BASE_API_URI + "plants?token=" + apiKey + "&page=" + page);
-        PlantListResponse response = restTemplate.getForObject(
+        PlantListResponse response;
+        URI listPlants;
+        if (orderFieldOne.equals("")) {
+            listPlants = URI.create(BASE_API_URI + "plants?token=" + apiKey + "&page=" + page);
+        } else {
+            listPlants = URI.create(BASE_API_URI + "plants?token=" + apiKey + "&page=" + page
+                    + "&order[" + orderFieldOne + "]=asc");
+        }
+        response = restTemplate.getForObject(
                 listPlants, PlantListResponse.class);
-        model.addAttribute("showImages", showImages);
         addPlantListToModel(model, response);
         return "listPlants";
     }
@@ -56,16 +72,31 @@ public class PlantListController {
         return "redirect:https://trefle.io/api/v1/plants/" + plantId + "?token=" + token;
     }
 
-    @RequestMapping(value = "/parse-pagination-link", method = RequestMethod.GET)
+    @RequestMapping(value = "/parse-pagination-link")
     public String parsePaginationLink(@RequestParam Map<String, String> query) {
-        String searchTerm = query.get("q");
+        if (query.containsKey("orderFieldOne")) {
+            return "redirect:/order-results?page=" + query.get("page") +
+                    (query.containsKey("q") ? "&q=" + query.get("q") : "")
+                    + "&showImages=" + query.get("showImages") + "&orderFieldOne=" + query.get("orderFieldOne");
+        }
         String page = query.get("page");
         String showImages = query.get("showImages");
-        if (searchTerm == null || searchTerm.equals("")) {
+        if (!query.containsKey("q")) {
             return "redirect:/plants?page=" + page + "&showImages=" + showImages;
-        }
-        else {
+        } else {
+            String searchTerm = query.get("q");
             return "redirect:/plants/search?page=" + page + "&q=" + searchTerm + "&showImages=" + showImages;
+        }
+    }
+
+    @RequestMapping(value = "/order-results")
+    public String orderResults(@RequestParam Map<String, String> query) {
+        if (!query.containsKey("q")) {
+            return "redirect:/plants?orderFieldOne=" + query.get("orderFieldOne")
+                    + "&showImages=" + query.get("showImages") + "&page=" + query.get("page");
+        } else {
+          return "redirect:/plants/search?q=" + query.get("q") + "&orderFieldOne=" + query.get("orderFieldOne")
+                  + "&showImages=" + query.get("showImages") + "&page=" + query.get("page");
         }
     }
 
@@ -79,7 +110,7 @@ public class PlantListController {
     }
 
     public static String getPage(String query) {
-        Pattern p = Pattern.compile("page=\\d+");
+        Pattern p = Pattern.compile("(?<=^|[?&])page=\\d+");
         Matcher m = p.matcher(query);
         boolean b = m.find();
         if (!b) {
@@ -89,32 +120,47 @@ public class PlantListController {
         }
     }
 
+    public static String getQ(String query) {
+        Pattern p = Pattern.compile("(?<=^|[?&])q=[^$&]+");
+        Matcher m = p.matcher(query);
+        boolean b = m.find();
+        if (!b) {
+            return "";
+        } else {
+            return m.group().replace("q=", "");
+        }
+    }
+
     public static void addPlantListToModel(Model model, PlantListResponse response) {
 
         if (response != null) {
             ListLinks links = response.getLinks();
             try {
                 URI trefle = new URI(API_SCHEME, API_HOST, null, null);
+                URI self = trefle.resolve(links.getSelf());
+                model.addAttribute("self", self);
+                model.addAttribute("selfLinkQuery", self.getRawQuery());
+                model.addAttribute("selfLinkPath", self.getPath());
 
                 URI first = trefle.resolve(links.getFirst());
-                model.addAttribute("firstLinkPath", first.getPath());
-                model.addAttribute("firstLinkQuery", first.getQuery());
+                model.addAttribute("firstLinkPath", first.getRawPath());
+                model.addAttribute("firstLinkQuery", first.getRawQuery());
 
                 URI last = trefle.resolve(links.getLast());
-                model.addAttribute("lastLinkPath", last.getPath());
-                model.addAttribute("lastLinkQuery", last.getQuery());
+                model.addAttribute("lastLinkPath", last.getRawPath());
+                model.addAttribute("lastLinkQuery", last.getRawQuery());
                 model.addAttribute("pageCount", getPage(last.getQuery()));
 
                 if (links.getPrev() != null) {
                     URI prev = trefle.resolve(links.getPrev());
-                    model.addAttribute("prevLinkPath", prev.getPath());
-                    model.addAttribute("prevLinkQuery", prev.getQuery());
+                    model.addAttribute("prevLinkPath", prev.getRawPath());
+                    model.addAttribute("prevLinkQuery", prev.getRawQuery());
                 }
 
                 if (links.getNext() != null) {
                     URI next = trefle.resolve(links.getNext());
-                    model.addAttribute("nextLinkPath", next.getPath());
-                    model.addAttribute("nextLinkQuery", next.getQuery());
+                    model.addAttribute("nextLinkPath", next.getRawPath());
+                    model.addAttribute("nextLinkQuery", next.getRawQuery());
                 }
             } catch (URISyntaxException e) {
                 model.addAttribute("error", "Bad URI");
--- src/main/java/org/launchcode/plantopedia/controllers/PlantSearchController.java
+++ src/main/java/org/launchcode/plantopedia/controllers/PlantSearchController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,17 +18,26 @@ public class PlantSearchController {
     @RequestMapping(value = "/plants/search")
     public String searchPlants(Model model, @RequestParam String q, HttpServletRequest request,
                                @RequestParam(defaultValue = "1") String page,
-                               @RequestParam(defaultValue = "1") Boolean showImages,
+                               @RequestParam(defaultValue = "1") String showImages,
+                               @RequestParam(name = "orderFieldOne", defaultValue = "") String orderFieldOne,
                                @Value("${TREFLE_API_TOKEN}") String apiKey) {
-
         RestTemplate restTemplate = new RestTemplate();
         String token = PlantListController.getClientToken(request.getRequestURI(), apiKey).getToken();
-        PlantListResponse response = restTemplate.getForObject(
-                BASE_API_URI + "plants/search?q=" + q + "&page=" + page + "&token=" + token,
-                PlantListResponse.class);
+        PlantListResponse response;
+        if (orderFieldOne.equals("")) {
+            response = restTemplate.getForObject(
+                    BASE_API_URI + "plants/search?q=" + q + "&page=" + page + "&token=" + token,
+                    PlantListResponse.class);
+        } else {
+            response = restTemplate.getForObject(
+                    BASE_API_URI + "plants/search?q=" + q + "&page=" + page + "&order[" + orderFieldOne + "]=asc" +
+                            "&token=" + token,
+                    PlantListResponse.class);
+        }
         model.addAttribute("q", q);
         model.addAttribute("page", page);
-        model.addAttribute("showImages", showImages);
+        model.addAttribute("showImages", !(showImages.equals("0") || showImages.equals("false")));
+        model.addAttribute("orderFieldOne", orderFieldOne);
         PlantListController.addPlantListToModel(model, response);
         return "listPlants";
     }
